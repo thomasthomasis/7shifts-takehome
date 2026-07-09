ba236e3ec99668542f6060923e33db87dddbfc68
Thomas Sloane <thomas.i.sloane@gmail.com>
2026-07-09 13:04:50 -0400

    Added basic readme and defined schemas for staff and shift objects

---
+++ backend/database/migrations/2026_07_09_165230_create_shifts_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('shifts', function (Blueprint $table) {
+            $table->id();
+            $table->date('day');
+            $table->dateTime('start_time');
+            $table->dateTime('end_time');
+            $table->string('role');
+            $table->foreignId('staff_id')->nullable()->constrained('staff')->nullOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('shifts');
+    }
+};
